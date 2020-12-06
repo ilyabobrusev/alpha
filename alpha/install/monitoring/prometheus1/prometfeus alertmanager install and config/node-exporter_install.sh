d6b76eb3c692946247efb780b3476080cfe98543
ilyabobrusev <qwesrt@rambler.ru>
2020-12-06 12:28:41 +0300

    jenkins and bitbucket

--- alpha/install/monitoring/prometheus1/prometfeus alertmanager install and config/node-exporter_install.sh
+++ alpha/install/monitoring/prometheus1/prometfeus alertmanager install and config/node-exporter_install.sh
@@ -1,11 +1,11 @@
- wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
- firewall-cmd --permanent --add-port=9100/tcp
- firewall-cmd --reload
- tar zxvf node_exporter-*.linux-amd64.tar.gz
- cd node_exporter-*.linux-amd64
- cp node_exporter /usr/local/bin/
- useradd --no-create-home --shell /bin/false nodeusr
- chown -R nodeusr:nodeusr /usr/local/bin/node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+firewall-cmd --permanent --add-port=9100/tcp
+firewall-cmd --reload
+tar zxvf node_exporter-*.linux-amd64.tar.gz
+cd node_exporter-*.linux-amd64
+cp node_exporter /usr/local/bin/
+useradd --no-create-home --shell /bin/false nodeusr
+chown -R nodeusr:nodeusr /usr/local/bin/node_exporter
 cat <<EOF | tee /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter Service
@@ -22,7 +22,7 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
- systemctl daemon-reload
- systemctl enable node_exporter
- systemctl start node_exporter
- systemctl status node_exporter
+systemctl daemon-reload
+systemctl enable node_exporter
+systemctl start node_exporter
+systemctl status node_exporter
